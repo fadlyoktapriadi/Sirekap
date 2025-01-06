fe2340306835af72a72368c60c3637594bfc132c
fadlyoktapriadi <fadlyoktapriadi41@gmail.com>
2025-01-06 16:19:09 +0700

    update database

--- sirekap.sql
+++ sirekap.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 02, 2025 at 10:29 AM
+-- Generation Time: Jan 06, 2025 at 10:18 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -75,7 +75,9 @@ CREATE TABLE `tbl_kerangka_kerja` (
 --
 
 INSERT INTO `tbl_kerangka_kerja` (`id_kak`, `program_kerja`, `nama_kegiatan`, `tanggal_mulai`, `tanggal_selesai`, `anggaran_dibutuhkan`, `anggaran_disetujui`, `penanggung_jawab`, `sasaran`, `target`, `file`, `status`, `created_at`, `updated_at`) VALUES
-(9, 'Penyuluhan Stunting', 'Imunisasi Vitamin B', '2025-01-01', '2025-01-31', 4500000, 4000000, '199207082001801901', 'Balita dan Anak dibawah 12 Tahun', 150, 'Imunisasi_Vitamin_B6775fd558b102.docx', 'Selesai', '2025-01-02', '2025-01-02');
+(9, 'Penyuluhan Stunting', 'Imunisasi Vitamin B', '2025-01-01', '2025-01-31', 4500000, 4000000, '199207082001801901', 'Balita dan Anak dibawah 12 Tahun', 150, 'Imunisasi_Vitamin_B6775fd558b102.docx', 'Selesai', '2025-01-02', '2025-01-02'),
+(11, 'test aaaaa', 'hahahhihihi', '2025-02-01', '2025-02-28', 5000000, 145000, '199207082001801901', 'asdfasdfasdf', 133, 'hahahhihihi677b5797c2cb1.pdf', 'Diterima', '2025-01-01', '2025-01-06'),
+(12, 'sfgadgasdg', 'sadgasdgasdg', '2025-03-01', '2025-03-31', 100000, NULL, '199207082001801901', 'sdafsdfasdfsd', 100, 'sadgasdgasdg677b581e39e3f.png', 'Diproses', '2025-01-06', '2025-01-06');
 
 -- --------------------------------------------------------
 
@@ -130,6 +132,27 @@ CREATE TABLE `tbl_lpj` (
 
 INSERT INTO `tbl_lpj` (`id_lpj`, `id_kak`, `anggaran_digunakan`, `keterangan`, `catatan`, `file_lpj`, `dokumentasi`, `created_at`, `updated_at`) VALUES
 (4, 9, 3500000, 'gak ada hehehe', 'aman hehe', 'LPJ_6775fe252b8d9.pdf', 'Dokumentasi_6775fe3a91543.jpg', '2025-01-02', '2025-01-02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pagu_anggaran`
+--
+
+CREATE TABLE `tbl_pagu_anggaran` (
+  `id_pagu_anggaran` int(11) NOT NULL,
+  `jumlah_anggaran` int(11) NOT NULL,
+  `tahun_anggaran` varchar(5) NOT NULL,
+  `created_at` date NOT NULL,
+  `updated_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_pagu_anggaran`
+--
+
+INSERT INTO `tbl_pagu_anggaran` (`id_pagu_anggaran`, `jumlah_anggaran`, `tahun_anggaran`, `created_at`, `updated_at`) VALUES
+(1, 25000000, '2025', '2025-01-06', '2025-01-06');
 
 -- --------------------------------------------------------
 
@@ -189,6 +212,12 @@ ALTER TABLE `tbl_lpj`
   ADD KEY `id_kak` (`id_kak`);
 
 --
+-- Indexes for table `tbl_pagu_anggaran`
+--
+ALTER TABLE `tbl_pagu_anggaran`
+  ADD PRIMARY KEY (`id_pagu_anggaran`);
+
+--
 -- Indexes for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
@@ -203,7 +232,7 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_kerangka_kerja`
 --
 ALTER TABLE `tbl_kerangka_kerja`
-  MODIFY `id_kak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_kak` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `tbl_kunjungan`
@@ -216,6 +245,12 @@ ALTER TABLE `tbl_kunjungan`
 --
 ALTER TABLE `tbl_lpj`
   MODIFY `id_lpj` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `tbl_pagu_anggaran`
+--
+ALTER TABLE `tbl_pagu_anggaran`
+  MODIFY `id_pagu_anggaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
